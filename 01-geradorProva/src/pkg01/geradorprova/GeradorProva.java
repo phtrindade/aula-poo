e057dcb11329a8b7e4ed0d801efa42dbd60d9b4f
Paulo H Trindade <phtrindade@edu.univali.br>
2018-09-04 01:28:02 -0300

    incompleto

--- 01-geradorProva/src/pkg01/geradorprova/GeradorProva.java
+++ 01-geradorProva/src/pkg01/geradorprova/GeradorProva.java
@@ -1,49 +1,134 @@
+
 /*
 Classe abstrata é uma classe não possuiu atributos necessários pra instanciar objeto tipo 
 veiculo, moto, carro, avião, não possibilatando representar um objeto sendo classes Pais
 veiculo pai de moto e carro, 
 static atributo compartilhado por varios objetos
 */
-package pkg01.geradorprova;
 
-/**
- *
- * @author Trindade
- */
+package pkg01.geradorprova;
+import java.util.Scanner;
 public class GeradorProva {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Prova x  = new Prova("POO");
-        x.setData("28-08-2018"); //Utilizando encapsulamento
-        x.setLocal("Lab-- 3 bloco B6");
-        // x.local ="Lab-- 3 bloco B6";
-        //x.data ="28-08-2018";
-       
+        Scanner recebe = new Scanner(System.in);
+         
+        //Scanner recebe = new Scanner(System.in);
+        System.out.print("Nome da Disciplina --> "); 
+        Prova p1 = new Prova(recebe.nextLine());
+        //p1.setNome(recebe.nextLine());
+        System.out.print("Local --> ");
+        p1.setLocal(recebe.nextLine());
+        System.out.print("Data --> ");
+        p1.setData(recebe.nextLine());
+        System.out.print("Peso --> ");
+        p1.setPeso(recebe.nextInt());
         
-        // metodo didático
-        String retornoDoMetodo = x.obtemDetalhes();
-        System.out.println(retornoDoMetodo);
-        //ou 
+        String aux = new String();
+        int qtdD;
+        do{
+            System.out.print("Numero de questões discursivas --> ");
+            qtdD = recebe.nextInt();
+            System.out.println("______________________________________________\n");
+            if(qtdD<0){
+                System.out.println("VALOR NECESSITA SER MAIOR QUE 'ZERO'");
+            }
+        }while(qtdD<0);
+        
+        
+        Discursiva qDisc [] = new Discursiva[qtdD]; // qDisc ->  questões discursivas
+        
+        for(int i=0;i<qtdD;i++){
+           qDisc[i] = new Discursiva();
+            System.out.print("Digite a Questão "+(i+1)+" -> ");
+            p1.recebe.nextLine();
+            //aux = recebe.nextLine();
+            //aux = recebe.nextLine();
+            qDisc[i].setPergunta(aux);   
+            System.out.print("Critério de Avaliação --> ");
+            aux = recebe.nextLine();
+            qDisc[i].setCriteriosCorrecao(aux);
+            System.out.print("Peso da questão --> ") ;
+            qDisc[i].setPeso(recebe.nextDouble());
+            System.out.println("______________________________________________");
+        }
+        p1.setDiscursiva(qDisc);
+        
+        
+        int qtdO;
+        do{
+            System.out.print("Numero de questões objetivas --> ");
+            qtdO = recebe.nextInt();
+            if(qtdO<0){
+                System.out.println("VALOR NECESSITA SER MAIOR QUE 'ZERO'");
+            }
+        }while(qtdO<0);
+        Objetiva qObj [] = new Objetiva[qtdO];
+        String [] c = new String[5];
+        for(int i=0;i<qtdO;i++){
+            qObj[i]= new Objetiva();
+            System.out.print("Digite a Questão "+(i+1)+" -> ");
+            aux = recebe.nextLine();
+            aux = recebe.nextLine();
+            qObj[i].setPergunta(aux); 
+            System.out.print("Peso --> ");
+            qObj[i].setPeso(recebe.nextDouble());
+            System.out.println("Entre com as Alternativas ");
+            for(int j=0;j<5;j++){
+                System.out.print((j+1)+".) alternativa: ");
+                aux = recebe.nextLine();
+                //calaux = recebe.nextLine();
+                c[j]=aux;
+            }
+            qObj[i].setOpcoes(c);
+            int alternativa;
+            do{
+                System.out.print("Alternativa correta é? ");
+                alternativa = recebe.nextInt();
+            }while(alternativa<0 || alternativa>6);
+            qObj[i].setRespostaCorreta(alternativa-1);                 
+        }
+        p1.setObjetiva(qObj);
+        
+
+        
+        System.out.println(p1.obtemDetalhes());
+        
+        /*Prova x = new Prova("POO");
+        x.setLocal("Lab 3 - Bloco B6");
+        x.setData("2018-08-28");
+        
+        //forma resumida
         System.out.println(x.obtemDetalhes());
         
-        
+        Discursiva d = new Discursiva();
+        d.setPergunta("Qual seu nome?");
+        d.setPeso(0);
+        d.setCriteriosCorrecao("Saber o próprio nome");
         
         Objetiva o = new Objetiva();
-        o.setPergunta("Qual a pergunta correta? ");
-        o.setPeso(3);
-        String[] opcoes =  new String[5];
-        opcoes[0]= "Gre";
-        opcoes[1]= "Corinthias";
-        opcoes[2]= "Palmeiras";
-        opcoes[3]= "Colorado";
-        opcoes[4]= "Bahia";
+        o.setPergunta("Qual o melhor time do Brasil?");
+        o.setPeso(2);
+        String[] opcoes = new String[5];
+        opcoes[0] = "Gremio";
+        opcoes[1] = "Inter";
+        opcoes[2] = "Flamento";
+        opcoes[3] = "Corinthians";
+        opcoes[4] = "Sao Paulo";
         o.setOpcoes(opcoes);
         o.setRespostaCorreta(3);
         
+                
+                
+        
+        // metodo didático
+       // String retornoDoMetodo = x.obtemDetalhes();
+       // System.out.println(retornoDoMetodo);
+        //ou 
+       // System.out.println(x.obtemDetalhes());
+        
+        */
+      
         
     }
     
--- 01-geradorProva/src/pkg01/geradorprova/Prova.java
+++ 01-geradorProva/src/pkg01/geradorprova/Prova.java
@@ -1,91 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg01.geradorprova;
-
-/**
- *
- * @author Trindade
- */
 public class Prova {
 
     private String nome;
     private int peso;
     private String local;
     private String data;
-
+    private Objetiva[] Objetiva;
+    private Discursiva[] Discursiva;
+    
     public String obtemDetalhes() {
-        String retorno = "";
-        retorno = retorno + "Nome: " + this.getNome() + "\n";
-        retorno = retorno + "Local: " + this.getLocal() + "\n";
-        retorno = retorno + "Data: " + this.getData() + "\n";
-        retorno = retorno + "Peso: " + this.getPeso() + "\n";
+        String retorno = "UNIVALI - Universidade do Vale do Itajaí\n";
+         retorno += "Nome: "  + this.getNome()  + "\n";
+         retorno += "Local: " + this.getLocal() + "\n";
+         retorno += "Data: "  + this.getData()  + "\n";
+         retorno += "Peso: "  + this.getPeso()  + "\n";
         return retorno;
     }
+    /*public String obtemProvaImpressao(){
+         String provaImpressao="Nome da Disciplina: "+this.getNome()+
+                 "\nPeso da Prova: "+this.getPeso()+
+                 "\nLocal da Prova: "+this.getLocal()+
+                 "\nData da Prova: "+this.getData()+
+                 "\n\n\t====Perguntas Discursivas=====\n";
+         
+         
+         //for (int i = 0; i < this..size(); i++) {
+         //    provaImpressao+="Questão "+(i+1)+") "+this...get(i).retornaQuestao();
+         //}
 
+         //return provaImpressao;
+     //}*/
     public Prova(String nome) {
         this.nome = nome;
         this.peso = 10;
-        //this.local = local;
-        //this.data = data;
+        
+//this.local = local;
+        //this.data = data;     
     }
 
-    /**
-     * @return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * @return the peso
-     */
     public int getPeso() {
         return peso;
     }
 
-    /**
-     * @param peso the peso to set
-     */
     public void setPeso(int peso) {
         this.peso = peso;
     }
 
-    /**
-     * @return the local
-     */
     public String getLocal() {
         return local;
     }
 
-    /**
-     * @param local the local to set
-     */
     public void setLocal(String local) {
         this.local = local;
     }
 
-    /**
-     * @return the data
-     */
     public String getData() {
         return data;
     }
 
-    /**
-     * @param data the data to set
-     */
     public void setData(String data) {
         this.data = data;
+    }
+
+    /**
+     * @return the Objetiva
+     */
+    public Objetiva[] getObjetiva() {
+        return Objetiva;
+    }
+
+    /**
+     * @param Objetiva the Objetiva to set
+     */
+    public void setObjetiva(Objetiva[] Objetiva) {
+        this.Objetiva = Objetiva;
+    }
+
+    /**
+     * @return the Discursiva
+     */
+    public Discursiva[] getDiscursiva() {
+        return Discursiva;
+    }
+
+    /**
+     * @param Discursiva the Discursiva to set
+     */
+    public void setDiscursiva(Discursiva[] Discursiva) {
+        this.Discursiva = Discursiva;
     }
 
 }
