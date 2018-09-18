88b5fe5904433704a433f177efdd5570c2a24df3
Paulo H Trindade <phtrindade@edu.univali.br>
2018-09-17 22:36:15 -0300

    faltando validação

--- geradorprova/Questao.java
+++ geradorprova/Questao.java
@@ -1,20 +1,39 @@
-package pkg01.geradorprova;
+
+package geradorprova;
+
+/**
+ *
+ * @author paulo
+ */
 abstract class Questao {
     private String pergunta;
     private double peso;
+    public abstract String retornaQuestao();
 
+    /**
+     * @return the pergunta
+     */
     public String getPergunta() {
         return pergunta;
     }
 
+    /**
+     * @param pergunta the pergunta to set
+     */
     public void setPergunta(String pergunta) {
         this.pergunta = pergunta;
     }
 
+    /**
+     * @return the peso
+     */
     public double getPeso() {
         return peso;
     }
 
+    /**
+     * @param peso the peso to set
+     */
     public void setPeso(double peso) {
         this.peso = peso;
     }
